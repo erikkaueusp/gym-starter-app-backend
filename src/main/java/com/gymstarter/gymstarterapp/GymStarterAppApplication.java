7ccadf9d23941530b170d756959e2b7bf8da9452
Erik <erikkaue@gmail.com>
2022-04-21 21:04:19 -0300

    Init project

---
+++ src/main/java/com/gymstarter/gymstarterapp/GymStarterAppApplication.java
@@ -0,0 +1,13 @@
+package com.gymstarter.gymstarterapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymStarterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymStarterAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gymstarter/gymstarterapp/GymStarterAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gymstarter.gymstarterapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymStarterAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
