526aa857a468cf079c8269a294ca409949f8b340
Erik Kaue <erikkaue@gmail.com>
2022-05-28 20:41:37 -0300

    removendo teste

--- src/main/java/com/gymstarter/gymstarterapp/GymStarterAppApplication.java
+++ src/main/java/com/gymstarter/gymstarterapp/GymStarterAppApplication.java
@@ -2,7 +2,7 @@ package com.gymstarter.gymstarterapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// Teste git
+
 @SpringBootApplication
 public class GymStarterAppApplication {
 
