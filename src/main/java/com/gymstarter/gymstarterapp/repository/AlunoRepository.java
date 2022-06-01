fd23f4e95b95c0a1efe1d082ac7565ac50733377
Erik Kaue <erikkaue@gmail.com>
2022-05-31 23:14:37 -0300

    moved

--- src/main/java/com/gymstarter/gymstarterapp/repository/AlunoRepository.java
+++ src/main/java/com/gymstarter/gymstarterapp/repository/AlunoRepository.java
@@ -1,6 +1,6 @@
 package com.gymstarter.gymstarterapp.repository;
 
-import com.gymstarter.gymstarterapp.entity.Aluno;
+import com.gymstarter.gymstarterapp.entity.aluno.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
