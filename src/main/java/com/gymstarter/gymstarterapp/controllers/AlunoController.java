deac9ba8a41cd0fb81e1491f60a4b9bc2f65da51
Erik Kaue <erikkaue@gmail.com>
2022-10-17 23:26:06 -0300

    mini refatorada no controller

--- src/main/java/com/gymstarter/gymstarterapp/controllers/AlunoController.java
+++ src/main/java/com/gymstarter/gymstarterapp/controllers/AlunoController.java
@@ -1,11 +1,16 @@
 package com.gymstarter.gymstarterapp.controllers;
 
+import com.gymstarter.gymstarterapp.FormAluno;
 import com.gymstarter.gymstarterapp.entity.aluno.model.AlunoModel;
 import com.gymstarter.gymstarterapp.services.AlunoService;
+import com.gymstarter.gymstarterapp.services.FotoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/aluno")
@@ -14,6 +19,19 @@ public class AlunoController {
     @Autowired
     private AlunoService service;
 
+    @Autowired
+    private FotoService fotoService;
+
+    @PostMapping
+    public ResponseEntity cadastraAluno(@RequestParam("foto") MultipartFile foto, @RequestParam("form") String form) throws IOException {
+
+        FormAluno formAluno = this.service.serializeFormAluno(form);
+        String url = fotoService.save(foto,formAluno);
+        service.save(formAluno,url);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+
+    }
 
     @GetMapping
     private ResponseEntity<AlunoModel> findAlunoByName(@RequestParam String nome) {
